f9d40ad3641bb94257ee91dd2ffa5b2b3dd5996e
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-07-02 00:24:39 -0500

    Correct dot tests

--- test/l2/dot_test.exs
+++ test/l2/dot_test.exs
@@ -174,6 +174,11 @@ defmodule Giraphe.L2.DotTest do
 
     assert digraph_from_switches(switches) ==
 """
+// Copyright © 2016 Jonathan Storm <the.jonathan.storm@gmail.com>
+// This work is free. You can redistribute it and/or modify it under the
+// terms of the Do What The Fuck You Want To Public License, Version 2,
+// as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
+
 digraph G {
   graph [
     outputorder=edgesfirst
--- test/l3/dot_test.exs
+++ test/l3/dot_test.exs
@@ -50,6 +50,11 @@ defmodule Giraphe.L3.DotTest do
 
     assert graph_from_routers(routers) ==
 """
+// Copyright © 2016 Jonathan Storm <the.jonathan.storm@gmail.com>
+// This work is free. You can redistribute it and/or modify it under the
+// terms of the Do What The Fuck You Want To Public License, Version 2,
+// as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
+
 graph G {
   graph [
     outputorder=edgesfirst
