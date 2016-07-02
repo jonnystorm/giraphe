22fdf73430f63d042d18e381e932eb21f6dc3ff5
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-07-02 16:08:09 -0500

    Fixing elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Giraphe.Mixfile do
   def project do
     [ app: :giraphe,
       version: "0.0.2",
-      elixir: "~> 1.3",
+      elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps
