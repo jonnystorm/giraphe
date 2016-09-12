ad209cf99d19c27743dbb1f0826d7f97a1d3524f
Jonathan Storm <the.jonathan.storm@gmail.com>
2016-09-12 15:01:38 -0500

    Update dependencies

--- dev_test.sh
+++ dev_test.sh
@@ -3,6 +3,6 @@
 while true; do
   inotifywait --exclude \..*\.sw. -re modify .
   clear
-  env MIX_ENV=prod mix escript.build
   mix test --include integrated
+  env MIX_ENV=prod mix escript.build
 done
